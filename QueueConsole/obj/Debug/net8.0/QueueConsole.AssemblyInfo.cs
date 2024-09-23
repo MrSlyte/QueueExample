817e26eaf1666d1f8a91ad165e8459d7e246d796
Leonardo Castellano <leonardo-r-castellano@outlook.com>
2024-09-23 19:37:24 -0300

    removed bin and obj

--- QueueConsole/obj/Debug/net8.0/QueueConsole.AssemblyInfo.cs
+++ QueueConsole/obj/Debug/net8.0/QueueConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d67d3a888b42d4c05c9dea45e19f56d377bce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
